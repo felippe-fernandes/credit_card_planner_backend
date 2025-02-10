5b66e1914361b39b0b24a6ac48fbd296c91ab3ff
Felippe Fernandes <felippe1000@hotmail.com>
2025-02-10 16:07:37 -0300

    Remove unused files and configurations: delete Prettier, TypeScript, and NestJS related files to clean up the codebase

--- src/card/card.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from '../auth/auth.module'; // Importe o módulo de autenticação
-
-@Module({
-  imports: [AuthModule], // Agora o JwtService estará disponível
-})
-export class CardModule {}
--- src/constants.ts
+++
@@ -1,6 +0,0 @@
-import { JwtSignOptions } from '@nestjs/jwt';
-
-export const SIGN_OPTIONS: JwtSignOptions = {
-  expiresIn: '7d',
-  audience: process.env.SUPABASE_URL,
-};
