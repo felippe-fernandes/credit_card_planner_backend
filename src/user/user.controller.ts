9bd17d4e071b2629150c84a63117f0dd734406fd
Felippe Fernandes <felippe1000@hotmail.com>
2025-03-30 20:57:00 -0300

    Add user-related DTOs and update UserController for enhanced user management

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,20 +12,37 @@ import {
   Res,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Role } from '@prisma/client';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Response } from 'express';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { AuthService } from 'src/auth/auth.service';
 import { RequestWithUser } from 'src/auth/interfaces/auth.interface';
 import { Roles } from 'src/auth/roles/roles.decorator';
 import { RolesGuard } from 'src/auth/roles/roles.guard';
-import { UpdateUserDto, UpdateUserRoleDto } from './dto/user.dto';
+import { ResponseNotFoundDto } from 'src/constants';
+import { ApiErrorDefaultResponses } from 'src/decorators/api-error-default-response.decorators';
+import {
+  ResultDeleteUserDto,
+  ResultFindAllUsersDto,
+  ResultFindOneUserDto,
+  ResultUpdateUserRoleDto,
+  UpdateUserDto,
+  UpdateUserRoleDto,
+} from './dto/user.dto';
 import { UserService } from './user.service';
 
 @ApiTags('Users')
 @ApiBearerAuth()
 @Controller('users')
+@ApiErrorDefaultResponses()
 @UseGuards(AuthGuard, RolesGuard)
 export class UserController {
   constructor(
@@ -36,57 +53,29 @@ export class UserController {
   @Get()
   @Roles('SUPER_ADMIN')
   @ApiOperation({ summary: 'Get all users' })
-  @ApiResponse({ status: 200, description: 'Users retrieved successfully.' })
-  @ApiResponse({ status: 404, description: 'No users found.' })
+  @ApiOkResponse({ type: ResultFindAllUsersDto })
+  @ApiNotFoundResponse({ type: ResponseNotFoundDto })
   findAll() {
     return this.userService.findAll();
-  }
-
-  @Get('me')
-  @ApiOperation({ summary: 'Get current logged in user' })
-  @ApiResponse({ status: 200, description: 'User retrieved successfully.' })
-  @ApiResponse({ status: 404, description: 'User not found.' })
-  findMe(@Req() req: RequestWithUser) {
-    return this.userService.findOne(req.user.id);
-  }
-
-  @Patch('me')
-  @ApiOperation({ summary: 'Update current logged in user' })
-  @ApiBody({ type: UpdateUserDto })
-  @ApiResponse({ status: 200, description: 'User updated successfully.' })
-  @ApiResponse({ status: 400, description: 'Failed to update user.' })
-  @ApiBody({ type: UpdateUserDto })
-  update(@Req() req: RequestWithUser, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(req.user.id, updateUserDto);
   }
 
   @Put('change-role')
   @Roles('SUPER_ADMIN')
   @ApiOperation({ summary: "Change a user's role" })
   @ApiBody({ type: UpdateUserRoleDto })
-  @ApiResponse({ status: 200, description: 'User role updated successfully.' })
-  @ApiResponse({ status: 400, description: 'Error updating user role.' })
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        userId: { type: 'string' },
-        newRole: { type: 'string', enum: Object.values(Role) },
-      },
-    },
-  })
+  @ApiOkResponse({ type: ResultUpdateUserRoleDto })
   updateRole(@Body() updateUserDto: UpdateUserRoleDto) {
     return this.userService.updateRole(updateUserDto);
   }
 
-  @Delete('me')
-  @ApiOperation({ summary: 'Delete current logged in user' })
-  @ApiResponse({ status: 200, description: 'User deleted successfully.' })
-  @ApiResponse({ status: 404, description: 'User not found.' })
-  async remove(@Req() req: RequestWithUser, @Res() res: Response) {
-    const userId = req.user.id;
+  @Delete('delete/:id')
+  @Roles('SUPER_ADMIN')
+  @ApiOperation({ summary: 'Delete a user by ID' })
+  @ApiOkResponse({ type: ResultDeleteUserDto })
+  @ApiParam({ name: 'userId', type: String, description: 'User ID' })
+  async deleteUser(@Param('userId') userId: string, @Res({ passthrough: true }) response: Response) {
     try {
-      await this.authService.deleteUser(userId, res);
+      await this.authService.deleteUser(userId, response);
       return this.userService.remove(userId);
     } catch (error) {
       console.error('Error deleting user:', error);
@@ -97,15 +86,39 @@ export class UserController {
     }
   }
 
-  @Delete('delete/:id')
+  @Patch('edit/:id')
   @Roles('SUPER_ADMIN')
-  @ApiOperation({ summary: 'Delete a user by ID' })
+  @ApiOperation({ summary: 'Update a user by ID' })
+  @ApiOkResponse({ type: ResultUpdateUserRoleDto })
   @ApiParam({ name: 'userId', type: String, description: 'User ID' })
-  @ApiResponse({ status: 200, description: 'User deleted successfully.' })
-  @ApiResponse({ status: 400, description: 'Error deleting user.' })
-  async deleteUser(@Param('userId') userId: string, @Res({ passthrough: true }) response: Response) {
+  async updateUser(@Param('userId') userId: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(userId, updateUserDto);
+  }
+
+  @Get('me')
+  @ApiOperation({ summary: 'Get current logged in user' })
+  @ApiOkResponse({ type: ResultFindOneUserDto })
+  @ApiNotFoundResponse({ type: ResponseNotFoundDto })
+  findMe(@Req() req: RequestWithUser) {
+    return this.userService.findOne(req.user.id);
+  }
+
+  @Patch('me')
+  @ApiOperation({ summary: 'Update current logged in user' })
+  @ApiBody({ type: UpdateUserDto })
+  @ApiOkResponse({ type: ResultUpdateUserRoleDto })
+  @ApiBody({ type: UpdateUserDto })
+  update(@Req() req: RequestWithUser, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(req.user.id, updateUserDto);
+  }
+
+  @Delete('me')
+  @ApiOperation({ summary: 'Delete current logged in user' })
+  @ApiOkResponse({ type: ResultDeleteUserDto })
+  async remove(@Req() req: RequestWithUser, @Res() res: Response) {
+    const userId = req.user.id;
     try {
-      await this.authService.deleteUser(userId, response);
+      await this.authService.deleteUser(userId, res);
       return this.userService.remove(userId);
     } catch (error) {
       console.error('Error deleting user:', error);
