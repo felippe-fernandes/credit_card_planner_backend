e7db458874aadfafb5905fd6b167a04698b62588
Felippe Fernandes <felippe1000@hotmail.com>
2025-03-30 14:56:01 -0300

    Enhance Dependent DTOs and Controller with detailed response structures and validation

--- src/dependents/dependents.controller.ts
+++ src/dependents/dependents.controller.ts
@@ -1,23 +1,47 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Req, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import {
   ApiBearerAuth,
-  ApiBody,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
   ApiParam,
   ApiQuery,
-  ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { RequestWithUser } from 'src/auth/interfaces/auth.interface';
-import { ReceivedDataDto } from 'src/constants';
+import { ResultDeleteCardDto } from 'src/cards/dto/cards.dto';
+import { ResponseNotFoundDto } from 'src/constants';
+import { ApiErrorDefaultResponses } from 'src/decorators/api-error-default-response.decorators';
 import { DependentsService } from './dependents.service';
-import { CreateDependentDto, FindAllDependentsDto, UpdateDependentDto } from './dto/dependents.dto';
+import {
+  CreateDependentDto,
+  FindAllDependentsDto,
+  ResultCreateDependentDto,
+  ResultFindAllDependentDto,
+  ResultFindOneDependentDto,
+  ResultUpdateDependentDto,
+  UpdateDependentDto,
+} from './dto/dependents.dto';
 
 @Controller('dependents')
 @ApiTags('Dependents')
 @ApiBearerAuth()
+@ApiErrorDefaultResponses()
 @UseGuards(AuthGuard)
 export class DependentsController {
   constructor(private readonly dependentsService: DependentsService) {}
@@ -26,8 +50,8 @@ export class DependentsController {
   @ApiOperation({ summary: 'Retrieve all dependents for a user' })
   @ApiQuery({ name: 'name', required: false, description: 'Filter dependents by name' })
   @ApiQuery({ name: 'id', required: false, description: 'Filter dependents by ID' })
-  @ApiOkResponse({ type: ReceivedDataDto })
-  @ApiResponse({ status: 404, description: 'No dependents found' })
+  @ApiOkResponse({ type: ResultFindAllDependentDto })
+  @ApiNotFoundResponse({ type: ResponseNotFoundDto })
   findAll(@Req() req: RequestWithUser, @Query('id') dependentId?: string, @Query('name') name?: string) {
     const filters: FindAllDependentsDto = {
       dependentId,
@@ -37,48 +61,30 @@ export class DependentsController {
     return this.dependentsService.findAll(userId, filters);
   }
 
+  @Post()
+  @ApiOperation({ summary: 'Create a new dependent' })
+  @ApiCreatedResponse({ type: ResultCreateDependentDto })
+  create(@Req() req: RequestWithUser, @Body() createDependentDto: CreateDependentDto) {
+    const userId = req.user.id;
+    return this.dependentsService.create(userId, createDependentDto);
+  }
+
   @Get('/search')
   @ApiOperation({ summary: 'Retrieve a specific dependent by ID or name' })
+  @ApiOkResponse({ type: ResultFindOneDependentDto })
+  @ApiNotFoundResponse({ type: ResponseNotFoundDto })
   @ApiQuery({ name: 'id', required: false, description: 'Dependent ID' })
   @ApiQuery({ name: 'name', required: false, description: 'Dependent name' })
-  @ApiResponse({ status: 200, description: 'Dependent retrieved successfully' })
-  @ApiResponse({ status: 404, description: 'Dependent not found' })
   findOne(@Req() req: RequestWithUser, @Query('id') id?: string, @Query('name') name?: string) {
     const userId = req.user.id;
     const filters = { id, name };
     return this.dependentsService.findOne(userId, filters);
   }
 
-  @Post()
-  @ApiOperation({ summary: 'Create a new dependent' })
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        name: { type: 'string' },
-      },
-    },
-  })
-  @ApiResponse({ status: 201, description: 'Dependent created successfully' })
-  @ApiResponse({ status: 400, description: 'Bad request' })
-  create(@Req() req: RequestWithUser, @Body() createDependentDto: CreateDependentDto) {
-    const userId = req.user.id;
-    return this.dependentsService.create(userId, createDependentDto);
-  }
-
   @Patch(':id')
   @ApiOperation({ summary: 'Update a dependent' })
+  @ApiOkResponse({ type: ResultUpdateDependentDto })
   @ApiParam({ name: 'id', description: 'Dependent ID' })
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        name: { type: 'string' },
-      },
-    },
-  })
-  @ApiResponse({ status: 200, description: 'Dependent updated successfully' })
-  @ApiResponse({ status: 404, description: 'Dependent not found' })
   update(
     @Req() req: RequestWithUser,
     @Param('id') id: string,
@@ -91,10 +97,15 @@ export class DependentsController {
   @Delete(':id')
   @ApiOperation({ summary: 'Delete a dependent' })
   @ApiParam({ name: 'id', description: 'Dependent ID' })
-  @ApiResponse({ status: 200, description: 'Dependent deleted successfully' })
-  @ApiResponse({ status: 404, description: 'Dependent not found' })
+  @ApiOkResponse({ type: ResultDeleteCardDto })
   remove(@Req() req: RequestWithUser, @Param('id') id: string) {
     const userId = req.user.id;
+    if (userId === id) {
+      throw new BadRequestException({
+        statusCode: HttpStatus.BAD_REQUEST,
+        message: 'User ID cannot be the same as the dependent ID',
+      });
+    }
     return this.dependentsService.remove(userId, id);
   }
 }
--- src/dependents/dto/dependents.dto.ts
+++ src/dependents/dto/dependents.dto.ts
@@ -1,13 +1,27 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { HttpStatus } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
+import { Dependent } from '@prisma/client';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { ResponseWithDataDto } from 'src/constants';
 
 export class CreateDependentDto {
+  @ApiProperty({
+    example: 'John Doe',
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
 }
 
-export class UpdateDependentDto extends PartialType(CreateDependentDto) {}
+export class UpdateDependentDto {
+  @ApiProperty({
+    example: 'John Doe',
+  })
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  name: string;
+}
 
 export class FindAllDependentsDto {
   @IsOptional()
@@ -27,4 +41,111 @@ export class FindOneDependentDto {
   @IsString()
   @IsOptional()
   name?: string;
+}
+
+const resultSearchDependent = [
+  {
+    id: 'a1b2c3d4-5678-90ab-cdef-1234567890ab',
+    userId: 'd5147b61-90d2-4b19-a987-c32e5e47e220',
+    name: 'John Doe',
+    createdAt: '2025-03-30T10:15:30.123Z',
+    editedAt: '2025-03-30T10:15:30.123Z',
+  },
+  {
+    id: 'f4e5d6c7-89ab-01cd-ef23-456789abcd12',
+    userId: 'd5147b61-90d2-4b19-a987-c32e5e47e220',
+    name: 'Emma Johnson',
+    createdAt: '2025-03-30T11:20:45.456Z',
+    editedAt: '2025-03-30T11:20:45.456Z',
+  },
+  {
+    id: '98ab76cd-5432-1ef0-9abc-345678def901',
+    userId: 'd5147b61-90d2-4b19-a987-c32e5e47e220',
+    name: 'Michael Williams',
+    createdAt: '2025-03-30T12:35:20.789Z',
+    editedAt: '2025-03-30T12:35:20.789Z',
+  },
+];
+
+export class ResultFindAllDependentDto extends ResponseWithDataDto {
+  @ApiProperty({
+    example: resultSearchDependent,
+  })
+  result: Dependent[];
+
+  @ApiProperty({
+    example: 3,
+  })
+  count: number;
+}
+
+export class ResultFindOneDependentDto extends ResponseWithDataDto {
+  @ApiProperty({
+    example: resultSearchDependent[0],
+  })
+  result: Dependent;
+
+  @ApiProperty({
+    example: 3,
+  })
+  count: number;
+}
+
+const resultCreateDependent = {
+  id: '98ab76cd-5432-1ef0-9abc-345678def901',
+  userId: 'd5147b61-90d2-4b19-a987-c32e5e47e220',
+  name: 'Michael Williams',
+  createdAt: '2025-03-30T12:35:20.789Z',
+  editedAt: '2025-03-30T12:35:20.789Z',
+};
+
+export class ResultCreateDependentDto extends ResponseWithDataDto {
+  @ApiProperty({
+    example: resultCreateDependent,
+  })
+  data: any;
+
+  @ApiProperty({
+    example: 1,
+  })
+  count: number;
+
+  @ApiProperty({
+    example: 201,
+  })
+  statusCode: HttpStatus;
+}
+
+export class ResultUpdateDependentDto extends ResponseWithDataDto {
+  @ApiProperty({
+    example: resultCreateDependent,
+  })
+  data: any;
+
+  @ApiProperty({
+    example: 1,
+  })
+  count: number;
+
+  @ApiProperty({
+    example: 200,
+  })
+  statusCode: HttpStatus;
+}
+
+export class ResultDeleteDependentDto extends ResponseWithDataDto {
+  @ApiProperty({
+    example: { dependentId: 'mb1231xcaasd1234da' },
+  })
+  data: any;
+
+  @ApiProperty({
+    example: 1,
+  })
+  count: number;
+
+  @ApiProperty({
+    example: 200,
+  })
+  statusCode: HttpStatus;
 }
