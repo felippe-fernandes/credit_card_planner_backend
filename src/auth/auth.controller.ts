328af021feb0c5733e0a9fad4abe18cf6b101718
Felippe Fernandes <felippe1000@hotmail.com>
2025-04-07 18:57:33 -0300

    Update API operation summaries to clarify user authentication context

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -26,44 +26,44 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  @ApiOperation({ summary: 'Login a user' })
+  @ApiOperation({ summary: 'Login a user', operationId: 'authLogin' })
   @ApiOkResponse({ type: ResultLoginDto })
-  async signIn(@Body() body: LoginDto, @Res({ passthrough: true }) response: Response) {
+  async authLogin(@Body() body: LoginDto, @Res({ passthrough: true }) response: Response) {
     return await this.authService.signIn(body, response);
   }
 
   @Post('signout')
-  @ApiOperation({ summary: 'Login a user' })
+  @ApiOperation({ summary: 'Sign out a user', operationId: 'authSignOut' })
   @ApiCreatedResponse({ type: ResponseCreatedDto })
-  async signOut(@Req() req: RequestWithUser, @Res({ passthrough: true }) response: Response) {
+  async authSignOut(@Req() req: RequestWithUser, @Res({ passthrough: true }) response: Response) {
     return await this.authService.signOut(response);
   }
 
   @Post('signup')
-  @ApiOperation({ summary: 'Create a new user' })
+  @ApiOperation({ summary: 'Create a new user', operationId: 'authSignUpUser' })
   @ApiCreatedResponse({ type: ResultSignupDto })
   @ApiExcludeEndpoint()
-  async signUpUser(@Body() body: SignupDto) {
+  async authSignUpUser(@Body() body: SignupDto) {
     return await this.authService.signUpUser(body);
   }
 
   @Post('signup/admin')
   @ApiBearerAuth()
   @Roles('SUPER_ADMIN')
-  @ApiOperation({ summary: 'Create a new admin' })
+  @ApiOperation({ summary: 'Create a new admin', operationId: 'authSignUpAdmin' })
   @ApiCreatedResponse({ type: ResultSignupDto })
   @ApiExcludeEndpoint()
-  async signUpAdmin(@Req() req: RequestWithUser, @Body() body: SignupDto) {
+  async authSignUpAdmin(@Req() req: RequestWithUser, @Body() body: SignupDto) {
     return await this.authService.signUpAdmin(body);
   }
 
   @Post('signup/super-admin')
   @ApiBearerAuth()
   @Roles('SUPER_ADMIN')
-  @ApiOperation({ summary: 'Create a new super admin' })
+  @ApiOperation({ summary: 'Create a new super admin', operationId: 'authSignUpSuperAdmin' })
   @ApiCreatedResponse({ type: ResultSignupDto })
   @ApiExcludeEndpoint()
-  async signUpSuperAdmin(@Body() body: SignupDto) {
+  async authSignUpSuperAdmin(@Body() body: SignupDto) {
     return await this.authService.signUpSuperAdmin(body);
   }
 }
--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -34,7 +34,7 @@ export class CardsController {
   constructor(private cardService: CardsService) {}
 
   @Get()
-  @ApiOperation({ summary: 'Retrieve all cards for the authenticated user' })
+  @ApiOperation({ summary: 'Retrieve all cards for the authenticated user', operationId: 'findAllCards' })
   @ApiOkResponse({ type: ResultFindAllCardDto })
   @ApiNotFoundResponse({ type: ResponseNotFoundDto })
   @ApiQuery({ name: 'flag', required: false, description: 'Card flag' })
@@ -42,7 +42,7 @@ export class CardsController {
   @ApiQuery({ name: 'dueDay', required: false, description: 'Due day' })
   @ApiQuery({ name: 'payDay', required: false, description: 'Pay day' })
   @ApiQuery({ name: 'name', required: false, description: 'Card name' })
-  async findAll(
+  async findAllCards(
     @Req() req: RequestWithUser,
     @Query('flag') flag?: string,
     @Query('bank') bank?: string,
@@ -58,21 +58,24 @@ export class CardsController {
   }
 
   @Post()
-  @ApiOperation({ summary: 'Create a new card for the authenticated user' })
+  @ApiOperation({ summary: 'Create a new card for the authenticated user', operationId: 'createCard' })
   @ApiCreatedResponse({ type: ResultCreateCardDto })
-  async create(@Req() req: RequestWithUser, @Body() createCardDto: CreateCardDto) {
+  async createCard(@Req() req: RequestWithUser, @Body() createCardDto: CreateCardDto) {
     const userId = req.user.id;
     return this.cardService.create(userId, createCardDto);
   }
 
   @Get('/search')
-  @ApiOperation({ summary: 'Retrieve a single card based on id or name. At least one parameter is required' })
+  @ApiOperation({
+    summary: 'Retrieve a single card based on id or name. At least one parameter is required',
+    operationId: 'findOneCard',
+  })
   @ApiOkResponse({ type: ResultFindOneCardDto })
   @ApiNotFoundResponse({ type: ResponseNotFoundDto })
   @ApiQuery({ name: 'id', required: false, description: 'Card ID' })
   @ApiQuery({ name: 'name', required: false, description: 'Card name' })
   @ApiQuery({ name: 'bank', required: false, description: 'Bank name' })
-  async findOne(
+  async findOneCard(
     @Req() req: RequestWithUser,
     @Query('id') id?: string,
     @Query('name') name?: string,
@@ -84,10 +87,10 @@ export class CardsController {
   }
 
   @Patch(':id')
-  @ApiOperation({ summary: 'Update an existing card' })
+  @ApiOperation({ summary: 'Update an existing card', operationId: 'updateCard' })
   @ApiOkResponse({ type: ResultUpdateCardDto })
   @ApiParam({ name: 'id', required: true, description: 'Card ID' })
-  async update(
+  async updateCard(
     @Req() req: RequestWithUser,
     @Param('id') cardId: string,
     @Body() updateCardDto: UpdateCardDto,
@@ -97,10 +100,10 @@ export class CardsController {
   }
 
   @Delete(':id')
-  @ApiOperation({ summary: 'Delete a card' })
+  @ApiOperation({ summary: 'Delete a card', operationId: 'deleteCard' })
   @ApiOkResponse({ type: ResultDeleteCardDto })
   @ApiParam({ name: 'id', required: true, description: 'Card ID' })
-  async remove(@Req() req: RequestWithUser, @Param('id') cardId: string) {
+  async deleteCard(@Req() req: RequestWithUser, @Param('id') cardId: string) {
     const userId = req.user.id;
     return this.cardService.remove(userId, cardId);
   }
